bad6b25b43e5fe257cb97de5e2ae6ade7ec4f1d9
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-06-19 11:27:05 +0530

    update home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import './Home.css'
+import Card6 from '../cards/Card6'
 import 'boxicons/css/boxicons.min.css';
 
 const Home = () => {
@@ -7,7 +8,22 @@ const Home = () => {
   const gridSize = 400;
   return (
     <div className='body'>
-      
+      <div className="container1 flex-b-c">
+        <div><h1>Hello Developers!!</h1></div>
+        <div><h2>The Best UI Collection For Your Websites</h2></div>
+        <div className="search-box">
+          <div className="icon flex-b-c"> <i className='bx bx-search'></i> </div>
+          <input type="text" placeholder='Search whatever you want to find'/>
+          <div className="search-btn flex-b-c"><button>Search</button></div>
+        </div>
+        <div className="shadow"></div>
+      </div>
+      <div className="container2 ">
+        <div className="box box1 flex-b-c"><Card6 /></div>
+        <div className="box box2 flex-b-c"><Card6 /></div>
+        <div className="box box3 flex-b-c"><Card6 /></div>
+        <div className="box box4 flex-b-c"><Card6 /></div>
+      </div>
     </div>
   )
 }
